bba0bf5b1bf4c91082ffede7705b1fa9a63b4379
Andrey Zakamaldin <zakamaldin.andrey@gmail.com>
2017-06-27 13:25:22 +0300

    First look 2.0

--- convert.py
+++ convert.py
@@ -2,5 +2,8 @@ from tkinter import *
 root = Tk()
 root.title('Перенос проекта')
 root.geometry('800x600')
-
+path = Label(root, text='Путь: ')
+path.place(x = 20, y = 30, width=120, height=25)
+msg = Message(root, bg = 'white', fg='black',width=600)
+msg.place(x = 10, y = 80, width=780, height=400)
 root.mainloop()
