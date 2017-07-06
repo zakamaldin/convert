74621970cdf200ddbf7c5b69f7c415f321994ba3
Zakamaldin Andrey <zakamaldin.andrey@gmail.com>
2017-07-06 10:56:12 +0300

    Change msg to listbox

--- convert.py
+++ convert.py
@@ -18,8 +18,8 @@ root.configure(background='gray93')
 pathLabel = Label(root,anchor=W, text='Путь: ', bg = 'gray93')
 pathLabel.place(x = 20, y = 30, width=400, height=25)
 
-msg = Message(root, bg = 'white', fg='black',width=600)
-msg.place(x = 10, y = 80, width=780, height=400)
+fileList = Listbox(root, bg = 'white', fg='black',width=600)
+fileList.place(x = 10, y = 80, width=780, height=400)
 
 butChoiceDir = Button(root,text ='Выбрать папку', command = choiceDir)
 butChoiceDir.place(x = 295, y = 500, width=100, height=25)
