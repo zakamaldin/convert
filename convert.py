6ea0dc18a035b9760c65a54c76da9058b0b78777
Zakamaldin Andrey <zakamaldin.andrey@gmail.com>
2017-07-06 15:20:52 +0300

    resize and status label with copy

--- convert.py
+++ convert.py
@@ -13,7 +13,6 @@ def choiceDir():
                 fileList.delete(0,END)
         files = os.listdir(pathDir)
         cpp = filter(lambda x: x.endswith('.cpp'), files)
-        print(cpp)
         for file in cpp:
                 fileList.insert(END,file)
         
@@ -21,12 +20,12 @@ def choiceDir():
 
 def startConvert():
         backUpDir = pathDir +"/BackUp/" + datetime.strftime(datetime.now(),"%Y.%m.%d")
-        status = Label(root, text='Перенос завершен', bg = 'gray93').place(x = 20, y = 550, width=400, height=25)
         if not os.path.exists(backUpDir):
                 os.makedirs(backUpDir)
                 if fileList.size() > 0:
                         items = fileList.get(0,END)
                 for index, item in enumerate(items):
+                        statusLabel['text'] = 'Копирование: ' +  item
                         copyFrom = pathDir + "/" + item
                         copyTo = backUpDir + "/" + item
                         shutil.copy(copyFrom,copyTo)
@@ -38,20 +37,27 @@ def startConvert():
 
 root = Tk()
 root.title('Перенос проекта')
-root.geometry('800x600')
+root.geometry('450x600')
 root.configure(background='gray93')
+root.resizable(width = False, height = False)
 
-pathLabel = Label(root,anchor=W, text='Путь: ', bg = 'gray93')
-pathLabel.place(x = 20, y = 30, width=400, height=25)
+pathLabel = Label(root, anchor = W, text = 'Путь: ', bg = 'gray93')
+pathLabel.place(x = 20, y = 30, width = 400, height = 25)
+statusLabel = Label(root,text ='', bg = 'gray93')
+statusLabel.place(x = 20, y = 550, width=400, height=25)
+scrollbar = Scrollbar(root)
+scrollbar.pack(side = RIGHT, fill = Y)
 
-fileList = Listbox(root, bg = 'white', fg='black',width=600)
-fileList.place(x = 10, y = 80, width=780, height=400)
+fileList = Listbox(root, yscrollcommand = scrollbar.set, bg = 'white', fg = 'black',width = 600)
+fileList.place(x = 10, y = 80, width = 415, height = 400)
+
+scrollbar.config(command = fileList.yview)
 
 butChoiceDir = Button(root,text ='Выбрать папку', command = choiceDir)
-butChoiceDir.place(x = 295, y = 500, width=100, height=25)
+butChoiceDir.place(x = 120, y = 500, width=100, height=25)
 
 butStartConvert = Button(root,text ='Начать перенос', command = startConvert)
-butStartConvert.place(x = 405, y = 500, width=100, height=25)
+butStartConvert.place(x = 230, y = 500, width=100, height=25)
 
 
 
